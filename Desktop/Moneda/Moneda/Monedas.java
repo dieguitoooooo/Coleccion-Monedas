b44848b4ec45ae695166304d079326b6cda24ada
diegoantonioalmontezarzuela@gmail.com <diegoantonioalmontezarzuela@gmail.com>
2018-01-24 11:17:16 +0100

    Hemos añadido la segunda clase monedas donde podemos añadir nuestra monedas a la coleccion y indicar por parametro el tipo, la cantidad y el año en el que se acuñó

--- Desktop/Moneda/Moneda/Monedas.java
+++ Desktop/Moneda/Moneda/Monedas.java
@@ -1,1 +1,29 @@
+import java.util.ArrayList;
+
+/**
+ * Write a description of class Monedas here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Monedas
+{
+    //aqui es donde se almacenaran mis monedas
+    private ArrayList<Moneda> coleccionMonedas;
+
+    /**
+     * Constructor for objects of class Monedas
+     */
+    public Monedas()
+    {
+        // inicializacion de mi nuevo objecto
+        coleccionMonedas = new ArrayList<Moneda>();
+    }
+    
+    public void addMoneda(String nombre, int cantidad, int anno)
+    {
+        // put your code here
+        coleccionMonedas.add(new Moneda(nombre, cantidad, anno));
+    }
+}
 
